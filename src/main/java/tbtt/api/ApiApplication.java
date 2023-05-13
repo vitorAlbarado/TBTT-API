eb35f4334036db37e09963e5990c3970e92650a3
Vitor Albarado da Silva <vitor.silva@inventuspower.com>
2023-05-13 01:50:40 -0400

    add cors configuration

--- src/main/java/tbtt/api/ApiApplication.java
+++ src/main/java/tbtt/api/ApiApplication.java
@@ -1,9 +1,11 @@
 package tbtt.api;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.SQLOutput;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -11,5 +13,9 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-	
+	@PostConstruct
+	public void init(){
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 }
