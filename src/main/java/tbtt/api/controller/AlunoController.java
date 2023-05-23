506850a69dc449cda491d2ffe5f0016df4fae8e5
Vitor Albarado <vitor.jardina@gmail.com>
2023-05-23 17:28:21 -0400

    add class service

--- src/main/java/tbtt/api/controller/AlunoController.java
+++ src/main/java/tbtt/api/controller/AlunoController.java
@@ -12,39 +12,38 @@ import tbtt.api.domain.model.aluno.AlunoDados;
 import tbtt.api.domain.Repository.AlunoRepository;
 import tbtt.api.domain.model.aluno.Aluno;
 import tbtt.api.domain.model.aluno.AlunoDadosAtualizados;
+import tbtt.api.domain.service.AlunoService;
 
 @RestController
 @RequestMapping("alunos")
 public class AlunoController {
 
     @Autowired
-    private AlunoRepository repository;
-
+    AlunoService alunoService;
     @PostMapping
     @Transactional
     public ResponseEntity cadastrar(@RequestBody @Valid AlunoDados dados, UriComponentsBuilder uriBuilder){
         var aluno = new Aluno(dados);
-        repository.save(aluno);
+        alunoService.save(aluno);
         var uri = uriBuilder.path("/alunos/{id}").buildAndExpand(aluno.getId()).toUri();
         return ResponseEntity.created(uri).body(new AlunoDados(aluno));
     }
 
    @GetMapping
     public ResponseEntity<Page<Aluno>> listar(Pageable pageable){
-
-        return ResponseEntity.ok(repository.findAll(pageable));
+        alunoService.findAll(pageable);
+        return ResponseEntity.ok(alunoService.findAll(pageable));
    }
    @GetMapping("/{id}")
    public ResponseEntity detalhaAluno (@PathVariable Long id){
-        var aluno = repository.getReferenceById(id);
+        var aluno = alunoService.findByID(id);
         return ResponseEntity.ok(new AlunoDados(aluno));
 
    }
    @PutMapping
    @Transactional
    public ResponseEntity atualizar(@RequestBody @Valid AlunoDadosAtualizados dados){
-    var aluno = repository.getReferenceById(dados.id());
-    aluno.atualizarDados(dados);
+    var aluno = alunoService.atualizar(dados);
     return ResponseEntity.ok(new AlunoDados(aluno));
    }
 
